f64f86792cd60d4705faf66734a94e480f97948b
emeka <maduekwepedro@gmail.com>
2022-01-10 03:02:18 +0100

    added other button widget to implement DRY principle

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Calculator',
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
@@ -33,11 +34,28 @@ class _MyHomePageState extends State<MyHomePage> {
   int _result = 0;
 
   void _incrementCounter(int keyPressed) {
-    print('hey $keyPressed');
+    print(
+      'hey $keyPressed',
+    );
+    print(keyPressed.runtimeType);
 
     setState(() {
       _result = _result + keyPressed;
     });
+  }
+
+  Widget buttonKeys(String buttonText, bool operator) {
+    return TextButton(
+        style: TextButton.styleFrom(
+          padding: const EdgeInsets.all(10.0),
+          backgroundColor: Color.fromRGBO(20, 20, 80, 1),
+          primary: Colors.white,
+          textStyle: const TextStyle(
+            fontSize: 20,
+          ),
+        ),
+        onPressed: () => _incrementCounter(7),
+        child: Text(buttonText));
   }
 
   @override
@@ -62,6 +80,13 @@ class _MyHomePageState extends State<MyHomePage> {
                   style: TextStyle(),
                 ),
               ),
+              Container(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  "clclc",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
               Spacer(),
               Container(
                 alignment: Alignment.center,
@@ -77,267 +102,45 @@ class _MyHomePageState extends State<MyHomePage> {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            padding: const EdgeInsets.all(10.0),
-                            backgroundColor: Color.fromRGBO(20, 20, 80, 1),
-                            primary: Colors.white,
-                            textStyle: const TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                          onPressed: () => _incrementCounter(7),
-                          child: const Text('7'),
-                        ),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            padding: const EdgeInsets.all(10.0),
-                            backgroundColor: Color.fromRGBO(20, 20, 80, 1),
-                            primary: Colors.white,
-                            textStyle: const TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                          onPressed: () => {print("hey")},
-                          child: const Text('8'),
-                        ),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            padding: const EdgeInsets.all(10.0),
-                            backgroundColor: Color.fromRGBO(20, 20, 80, 1),
-                            primary: Colors.white,
-                            textStyle: const TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                          onPressed: () => {print("hey")},
-                          child: const Text('9'),
-                        ),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            padding: const EdgeInsets.all(10.0),
-                            backgroundColor: Color.fromRGBO(20, 20, 80, 1),
-                            primary: Colors.white,
-                            textStyle: const TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                          onPressed: () => {print("hey")},
-                          child: const Text('('),
-                        ),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            padding: const EdgeInsets.all(10.0),
-                            backgroundColor: Color.fromRGBO(20, 20, 80, 1),
-                            primary: Colors.white,
-                            textStyle: const TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                          onPressed: () => {print("hey")},
-                          child: const Text(')'),
-                        )
+                        buttonKeys('7'),
+                        buttonKeys('8'),
+                        buttonKeys('9'),
+                        buttonKeys('('),
+                        buttonKeys(')'),
                       ],
                     ),
 // Expanded()
                     SizedBox(height: 30),
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            padding: const EdgeInsets.all(10.0),
-                            backgroundColor: Color.fromRGBO(20, 20, 80, 1),
-                            primary: Colors.white,
-                            textStyle: const TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                          onPressed: () => {print("hey")},
-                          child: const Text('4'),
-                        ),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            padding: const EdgeInsets.all(10.0),
-                            backgroundColor: Color.fromRGBO(20, 20, 80, 1),
-                            primary: Colors.white,
-                            textStyle: const TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                          onPressed: () => {print("hey")},
-                          child: const Text('5'),
-                        ),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            padding: const EdgeInsets.all(10.0),
-                            backgroundColor: Color.fromRGBO(20, 20, 80, 1),
-                            primary: Colors.white,
-                            textStyle: const TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                          onPressed: () => {print("hey")},
-                          child: const Text('6'),
-                        ),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            padding: const EdgeInsets.all(10.0),
-                            backgroundColor: Color.fromRGBO(20, 20, 80, 1),
-                            primary: Colors.white,
-                            textStyle: const TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                          onPressed: () => {print("hey")},
-                          child: const Text('*'),
-                        ),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            padding: const EdgeInsets.all(10.0),
-                            backgroundColor: Color.fromRGBO(20, 20, 80, 1),
-                            primary: Colors.white,
-                            textStyle: const TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                          onPressed: () => {print("hey")},
-                          child: const Text('/'),
-                        )
-                      ],
-                    ),
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          buttonKeys('4'),
+                          buttonKeys('5'),
+                          buttonKeys('6'),
+                          buttonKeys('*'),
+                          buttonKeys('/')
+                        ]),
                     SizedBox(height: 30),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            padding: const EdgeInsets.all(10.0),
-                            backgroundColor: Color.fromRGBO(20, 20, 80, 1),
-                            primary: Colors.white,
-                            textStyle: const TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                          onPressed: () => {print("hey")},
-                          child: const Text('1'),
-                        ),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            padding: const EdgeInsets.all(10.0),
-                            backgroundColor: Color.fromRGBO(20, 20, 80, 1),
-                            primary: Colors.white,
-                            textStyle: const TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                          onPressed: () => {print("hey")},
-                          child: const Text('2'),
-                        ),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            padding: const EdgeInsets.all(10.0),
-                            backgroundColor: Color.fromRGBO(20, 20, 80, 1),
-                            primary: Colors.white,
-                            textStyle: const TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                          onPressed: () => {print("hey")},
-                          child: const Text('3'),
-                        ),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            padding: const EdgeInsets.all(10.0),
-                            backgroundColor: Color.fromRGBO(20, 20, 80, 1),
-                            primary: Colors.white,
-                            textStyle: const TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                          onPressed: () => {print("hey")},
-                          child: const Text('+'),
-                        ),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            padding: const EdgeInsets.all(10.0),
-                            backgroundColor: Color.fromRGBO(20, 20, 80, 1),
-                            primary: Colors.white,
-                            textStyle: const TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                          onPressed: () => {print("hey")},
-                          child: const Text('-'),
-                        )
+                        buttonKeys('1'),
+                        buttonKeys('2'),
+                        buttonKeys('3'),
+                        buttonKeys('+'),
+                        buttonKeys('-'),
                       ],
                     ),
-// 0 . +/- exp =
                     SizedBox(height: 30),
 
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            padding: const EdgeInsets.all(10.0),
-                            backgroundColor: Color.fromRGBO(20, 20, 80, 1),
-                            primary: Colors.white,
-                            textStyle: const TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                          onPressed: () => {print("hey")},
-                          child: const Text('0'),
-                        ),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            padding: const EdgeInsets.all(10.0),
-                            backgroundColor: Color.fromRGBO(20, 20, 80, 1),
-                            primary: Colors.white,
-                            textStyle: const TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                          onPressed: () => {print("hey")},
-                          child: const Text('.'),
-                        ),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            padding: const EdgeInsets.all(10.0),
-                            backgroundColor: Color.fromRGBO(20, 20, 80, 1),
-                            primary: Colors.white,
-                            textStyle: const TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                          onPressed: () => {print("hey")},
-                          child: const Text('+/-'),
-                        ),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            padding: const EdgeInsets.all(10.0),
-                            backgroundColor: Color.fromRGBO(20, 20, 80, 1),
-                            primary: Colors.white,
-                            textStyle: const TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                          onPressed: () => {print("hey")},
-                          child: const Text('exp'),
-                        ),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            padding: const EdgeInsets.all(10.0),
-                            backgroundColor: Color.fromRGBO(20, 20, 80, 1),
-                            primary: Colors.white,
-                            textStyle: const TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                          onPressed: () => {print("hey")},
-                          child: const Text('='),
-                        )
+                        buttonKeys('0'),
+                        buttonKeys('.'),
+                        buttonKeys('+/-'),
+                        buttonKeys('exp'),
+                        buttonKeys('='),
                       ],
                     ),
                   ],
